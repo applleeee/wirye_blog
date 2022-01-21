b3412c34090dbc55947baeda58f7ff7dda31b3b9
applleeee <warm9806@gmail.com>
2022-01-21 20:09:11 +0900

    1/21

--- 위례2.py
+++ 위례2.py
@@ -1,127 +1,84 @@
+from textwrap import fill
+from turtle import color
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import csv
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+import datetime
+
+# 키워드 기존 통계파일에서 불러오기
+wb = load_workbook("위례블로그 통계(국어,수학).xlsx")
+ws1 = wb["국어키워드"]
+ws2 = wb["수학키워드"]
+
+# 키워드(국어, 수학 따로)
+ko_keywords = [] 
+math_keywords = []
+
+
+for word in ws1.iter_rows(min_row=2, min_col=5):
+    if word[0].value is not None:
+        ko_keyword = word[0].value
+        ko_keywords.append(ko_keyword)
+
+for word in ws2.iter_rows(min_row=2, min_col=5):
+    if word[0].value is not None:
+        math_keyword = word[0].value
+        math_keywords.append(math_keyword)
+
 
 url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query="
-keywords = [
-'위례고국어',
-'위례고국어내신',
-'한빛고국어',
-'한빛고국어내신',
-'덕수고국어',
-'덕수고국어내신',
-'위례덕수고 국어',
-'위례중앙중 국어',
-'위례중앙중학교 국어',
-'위례중앙중 국어학원',
-'한빛중 국어',
-'위례한빛중 국어',
-'한빛중 국어학원',
-'위례중 국어',
-'위례중학교 국어',
-'위례중 국어학원',
-'송례중 국어',
-'위례송례중 국어',
-'송례중 국어학원',
-'위례국어',
-'위례국어학원',
-'위례고등국어',
-'위례고등국어학원',
-'위례중등국어',
-'위례중등국어학원',
-'위례수능국어',
-'위례수능국어학원',
-'위례독서논술',
-'위례중앙타워 국어',
-'위례중앙타워 국어학원',
-'위례중앙타워 송현국어',
-'위례동국어',
-'위례동국어학원',
-'성남국어',
-'성남국어학원',
-'수정구국어',
-'수정구국어학원',
-'창곡동국어',
-'창곡동국어학원',
-'장지동국어',
-'장지동국어학원',
-'위례고수학',
-'위례고수학내신',
-'한빛고수학',
-'한빛고수학내신',
-'덕수고수학',
-'덕수고수학내신',
-'위례덕수고 수학',
-'위례중앙중 수학',
-'위례중앙중학교 수학',
-'위례중앙중 수학학원',
-'한빛중 수학',
-'위례한빛중 수학',
-'한빛중 수학학원',
-'위례중 수학',
-'위례중학교 수학',
-'위례중 수학학원',
-'송례중 수학',
-'위례송례중 수학',
-'송례중 수학학원',
-'위례수학',
-'위례수학학원',
-'위례고등수학',
-'위례고등수학학원',
-'위례중등수학',
-'위례중등수학학원',
-'위례초등수학',
-'위례초등수학학원',
-'위례경시',
-'위례경시대회',
-'위례영재교육',
-'위례중앙타워 수학',
-'위례중앙타워 수학학원',
-'위례중앙타워 하늘교육',
-'위례중앙타워 학원',
-'위례동수학',
-'위례동수학학원',
-'성남수학',
-'성남수학학원',
-'수정구수학',
-'수정구수학학원',
-'창곡동수학',
-'창곡동수학학원',
-'장지동수학',
-'장지동수학학원',
-]
 
-browser = webdriver.Chrome("C:/Users/user/Naver MYBOX/코딩/python/naver_blog/chromedriver.exe")
+browser = webdriver.Chrome("C:/Users/user/Desktop/코딩/wirye_blog/chromedriver.exe")
 browser.get(url)
 
-file = open("위례키워드.csv", encoding="utf-8-sig", newline="", mode="w")
-writer = csv.writer(file)
-writer.writerow(["검색어", "순위", "글제목"])
+
+# 검색 및 결과값 입력
+def input_result(keyword, sheet):
+    rank = []
+    title = []
+    for keyword1 in keyword:
+        elem = browser.find_element_by_id("nx_query")
+        elem.clear()
+        elem.send_keys(keyword1)
+        elem = browser.find_element_by_class_name("bt_search")
+        elem.click()
+        soup = BeautifulSoup(browser.page_source, "lxml")
+
+        blog = soup.find("a", {"href": "https://blog.naver.com/wredusky"})
+        if blog:
+            print("검색중")
+            li = blog.find_parent("li")
+            rank1 = blog.find_parent("li")["data-cr-rank"]
+            title1 = li.find("a", {"class" : "api_txt_lines total_tit _cross_trigger"}).text
+            rank.append(rank1)
+            title.append(title1)
+            
+        else:
+            print("순위없음")
+            
+            rank.append("순위없음")
+            title.append("게시물 없음")
+
+    # 오늘날짜 입력
+    sheet["g1"] = str(datetime.datetime.today()) 
+
+    # 순위, 제목 입력
+    for index, cell in enumerate(sheet.iter_rows(min_col=5, min_row=2)):
+        
+        cell[2].value = rank[index]
+        cell[5].value = title[index]
+
+    wb.save("blog_keyword.xlsx")
 
 
-for keyword in keywords:
-    a = []
-    elem = browser.find_element_by_id("nx_query")
-    elem.clear()
-    elem.send_keys(keyword)
-    elem = browser.find_element_by_class_name("bt_search")
-    elem.click()
-    soup = BeautifulSoup(browser.page_source, "lxml")
+input_result(ko_keywords, ws1)
+input_result(math_keywords, ws2)
 
-    blog = soup.find("a", {"href": "https://blog.naver.com/wredusky"})
-    if blog:
-        print("검색중")
-        li = blog.find_parent("li")
-        rank = blog.find_parent("li")["data-cr-rank"]
-        title = li.find("a", {"class" : "api_txt_lines total_tit _cross_trigger"}).text
-        dic1 = {"keyword" : keyword, "rank" : rank, "title" : title}
-        a.append(dic1)
-        for b in a:
-            writer.writerow(b.values())
 
-    else:
-        print("순위없음")
-        dic2 = {"keyword" : keyword, "rank" : "순위없음", "title" : "게시물없음"}
-        a.append(dic2)
-        for c in a:
-            writer.writerow(c.values())
+for a in ws1.iter_rows(min_col=5,min_row=2):
+    if type(a[2].value) == str or int(a[2].value) > 5:
+        a[0].fill = PatternFill(fgColor="FF6D6D", fill_type="solid")
+    elif int(a[2].value) <= 5:
+        a[0].fill = PatternFill(fgColor="D9E1F2", fill_type="solid")
+wb.save("blog_keyword.xlsx")
